c31b78b4bb2c8e225bf634d2cdfc488aa6224239
mrstnj <work297917@gmail.com>
2023-02-14 13:15:14 +0000

    add:セキュリティ機能の追加

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,23 @@
 class FoodsController < ApplicationController
+  before_action :logged_in_user
+  before_action :correct_user
+
   def new
   end
+
+  private
+
+  # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:user_id])
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
 end
