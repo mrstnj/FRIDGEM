d3d4c3d68a51987d4f57580cc9d436cfa06436b0
mrstnj <work297917@gmail.com>
2023-02-22 14:50:29 +0000

    update:微修正

--- db/migrate/20230222131302_remove_start_time_from_food_consumes.rb
+++ db/migrate/20230222131302_remove_start_time_from_food_consumes.rb
@@ -1,5 +1,5 @@
 class RemoveStartTimeFromFoodConsumes < ActiveRecord::Migration[7.0]
   def change
-    remove_column :food_consumes, :start_time, :date
+    remove_column :food_consumes, :consume_date, :date
   end
 end
