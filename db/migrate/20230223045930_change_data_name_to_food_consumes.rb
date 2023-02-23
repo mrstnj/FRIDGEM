820dda38837fa0b9aaec08130fc339102567132f
mrstnj <work297917@gmail.com>
2023-02-23 05:21:22 +0000

    fix:int型変換のマイグレーション

--- db/migrate/20230223045930_change_data_name_to_food_consumes.rb
+++ db/migrate/20230223045930_change_data_name_to_food_consumes.rb
@@ -1,5 +1,5 @@
 class ChangeDataNameToFoodConsumes < ActiveRecord::Migration[7.0]
   def change
-    change_column :food_consumes, :name, :integer
+    change_column :food_consumes, :name, 'integer USING CAST(name AS integer)'
   end
 end
